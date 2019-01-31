78033ed659d31a8f474620a7501d9167c42b544e
xxjokerx <rouzic.jeremie@gmail.com>
2019-01-31 11:41:01 +0100

    Completed pom.xml to add mapstruct, wsdl4j and jaxb2 to the project

--- src/main/java/com/gg/proj/app/LibraryServiceApplication.java
+++ src/main/java/com/gg/proj/app/LibraryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.gg.proj;
+package com.gg.proj.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/gg/proj/app/ServletInitializer.java
+++ src/main/java/com/gg/proj/app/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.gg.proj;
+package com.gg.proj.app;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
